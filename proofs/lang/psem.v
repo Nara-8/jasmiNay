5c8b22cc4fbe32421ce57874c73b62cea0976e63
Paolo Torrini <ptorrini@sta-04020061.inria.fr>
2025-04-01 16:09:52 +0200

    made it_sems_core.v independent from psem.v, by moving a few definitions from psem.v to psem_def.v

--- proofs/lang/psem.v
+++ proofs/lang/psem.v
@@ -93,39 +93,6 @@ End ESTATE_UTILS.
 (* ** Instructions
  * -------------------------------------------------------------------- *)
 
-Class semCallParams
-  {syscall_state : Type}
-  {ep : EstateParams syscall_state}
-  {scs : syscall_sem syscall_state}
-  {pT : progT}
-  := SemCallParams
-  {
-  init_state : extra_fun_t -> extra_prog_t -> extra_val_t -> estate -> exec estate;
-  finalize   : extra_fun_t -> mem -> mem;
-  exec_syscall : syscall_state_t -> mem -> syscall_t -> values -> exec (syscall_state_t * mem * values);
-  exec_syscallP: forall scs m o vargs vargs' rscs rm vres,
-     exec_syscall scs m o vargs = ok (rscs, rm, vres) ->
-     List.Forall2 value_uincl vargs vargs' ->
-     exists2 vres', exec_syscall scs m o vargs' = ok (rscs, rm, vres') & List.Forall2 value_uincl vres vres';
-  exec_syscallS: forall scs m o vargs rscs rm vres,
-     exec_syscall scs m o vargs = ok (rscs, rm, vres) ->
-     mem_equiv m rm;
-}.
-
-(** Switch for the semantics of function calls:
-  - when false, arguments and returned values are truncated to the declared type of the called function;
-  - when true, arguments and returned values are allowed to be undefined.
-
-Informally, “direct call” means that passing arguments and returned value does not go through an assignment;
-indeed, assignments truncate and fail on undefined values.
-*)
-Class DirectCall := {
-  direct_call : bool;
-}.
-
-Definition indirect_c : DirectCall := {| direct_call := false |}.
-Definition direct_c : DirectCall := {| direct_call := true |}.
-
 Section SEM.
 
 Context
@@ -143,10 +110,6 @@ Notation gd := (p_globs P).
 
 Definition get_lvar_i (x: lval) : exec var_i :=
   if x is Lvar x then ok x else type_error.
-
-Definition dc_truncate_val t v :=
-  if direct_call then ok v
-  else truncate_val t v.
 
 Inductive sem : estate -> cmd -> estate -> Prop :=
 | Eskip s :
--- proofs/lang/psem_defs.v
+++ proofs/lang/psem_defs.v
@@ -216,6 +216,61 @@ Definition sem_sopn gd o m lvs args :=
 
 End EXEC_ASM.
 
+(***** Moved here from psem.v *)
+
+Class semCallParams
+  {syscall_state : Type}
+  {ep : EstateParams syscall_state}
+  {scs : syscall_sem syscall_state}
+  {pT : progT}
+  := SemCallParams
+  {
+  init_state : extra_fun_t -> extra_prog_t -> extra_val_t -> estate -> exec estate;
+  finalize   : extra_fun_t -> mem -> mem;
+  exec_syscall : syscall_state_t -> mem -> syscall_t -> values -> exec (syscall_state_t * mem * values);
+  exec_syscallP: forall scs m o vargs vargs' rscs rm vres,
+     exec_syscall scs m o vargs = ok (rscs, rm, vres) ->
+     List.Forall2 value_uincl vargs vargs' ->
+     exists2 vres', exec_syscall scs m o vargs' = ok (rscs, rm, vres') & List.Forall2 value_uincl vres vres';
+  exec_syscallS: forall scs m o vargs rscs rm vres,
+     exec_syscall scs m o vargs = ok (rscs, rm, vres) ->
+     mem_equiv m rm;
+}.
+
+(** Switch for the semantics of function calls:
+  - when false, arguments and returned values are truncated to the declared type of the called function;
+  - when true, arguments and returned values are allowed to be undefined.
+
+Informally, “direct call” means that passing arguments and returned value does not go through an assignment;
+indeed, assignments truncate and fail on undefined values.
+*)
+Class DirectCall := {
+  direct_call : bool;
+}.
+
+Definition indirect_c : DirectCall := {| direct_call := false |}.
+Definition direct_c : DirectCall := {| direct_call := true |}.
+
+Section SEM0.
+Context
+  {dc:DirectCall}
+  {asm_op syscall_state : Type}
+  {ep : EstateParams syscall_state}
+  {spp : SemPexprParams}
+  {sip : SemInstrParams asm_op syscall_state}
+  {pT : progT}
+  {scP : semCallParams}
+  (P : prog)
+  (ev : extra_val_t).
+
+Definition dc_truncate_val t v :=
+  if direct_call then ok v
+  else truncate_val t v.
+
+End SEM0.
+
+(******************)
+
 End WSW.
 
 (* Just for extraction *)
